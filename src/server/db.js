f8c5eeba33ab7907a583a096a8957a656a1e0ca9
Roy Brown <roybrown77@gmail.com>
2019-04-18 02:25:08 -0400

    update

--- src/server/db.js
+++ src/server/db.js
@@ -1,3 +1,2 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://admin:admin@ds129183.mlab.com:29183/react-template', { useNewUrlParser: true });
