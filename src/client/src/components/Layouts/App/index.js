d9de6af0249cefaf2abccb927c70bf295e5250fe
roybrown77 <roybrown77@gmail.com>
2019-09-13 16:55:22 -0400

    remove deprecated code

--- src/client/src/components/Layouts/App/index.js
+++ src/client/src/components/Layouts/App/index.js
@@ -25,7 +25,7 @@ const styles = theme => ({
         minHeight: '0'
     },
     icon: {
-        marginRight: theme.spacing.unit * 2,
+        marginRight: theme.spacing(2),
     },
     heroUnit: {
         backgroundColor: theme.palette.background.paper,
@@ -33,30 +33,30 @@ const styles = theme => ({
     heroContent: {
         maxWidth: 600,
         margin: '0 auto',
-        padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+        padding: `${theme.spacing(8)}px 0 ${theme.spacing(6)}px`,
     },
     heroButtons: {
-        marginTop: theme.spacing.unit * 4,
+        marginTop: theme.spacing(4),
     },
     layout: {
         width: 'auto',
-        marginLeft: theme.spacing.unit * 3,
-        marginRight: theme.spacing.unit * 3,
-        [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+        marginLeft: theme.spacing(3),
+        marginRight: theme.spacing(3),
+        [theme.breakpoints.up(1100 + theme.spacing(6))]: {
             width: 1100,
             marginLeft: 'auto',
             marginRight: 'auto',
         },
     },
     cardGrid: {
-        padding: `${theme.spacing.unit * 8}px 0`,
+        padding: `${theme.spacing(8)}px 0`,
     },
     cardMedia: {
         padding: '50%', // 16:9
     },
     footer: {
         backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing.unit * 6,
+        padding: theme.spacing(6),
     },
     navigationUnselected: {
         fontWeight: '600',
