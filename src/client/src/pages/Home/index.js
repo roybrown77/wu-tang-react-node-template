c9a4b65597e3dfd4b34845f48b28c62d03ddbc6b
roybrown77 <roybrown77@gmail.com>
2019-09-15 08:56:03 -0400

    update home page; load wu bangers

--- src/client/src/pages/Home/index.js
+++ src/client/src/pages/Home/index.js
@@ -329,7 +329,7 @@ class Home extends React.Component {
                       dataLoading && 
                       <div style={{flexGrow: 1,padding:'1rem'}}>
                         <Typography align="center" color="textSecondary" paragraph>
-                          Takes a minute to screen scrape wikipedia for images so watch youtube or netflix or something.  :D
+                          Takes a minute to screen scrape wikipedia for images so watch youtube or netflix.  :D
                         </Typography>
                         <LinearProgress />
                         <br />
