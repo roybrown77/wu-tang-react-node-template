2d9b8c4110d121e38e6ca4438de8da37108b6af3
roybrown77 <roybrown77@gmail.com>
2019-11-12 08:13:08 -0500

    add unit tests

---
+++ src/client/src/reducers/albumReducer.test.js
@@ -0,0 +1,21 @@
+import {albumReducer} from './albumReducer';
+import {albumActionTypes} from '../constants/actionTypes';
+
+const initialState = {items: [], dataLoading: false};
+
+describe('albumReducer', () => {
+	describe('get album covers', () => {
+		it('with undefined state and action type should return initial state', async () => {
+			const action = {type:'ALBUMCOVERS_REQUEST'};
+			const response = albumReducer(undefined,{});
+			expect(response).toEqual(initialState);
+		});
+
+		it('with undefined state and action type ALBUMCOVERS_REQUEST', async () => {
+			const action = {type:'ALBUMCOVERS_REQUEST'};
+			const response = albumReducer(undefined,action);
+			console.log(response);
+			expect(response).toEqual({...initialState, dataLoading: true});
+		});
+	});
+});
