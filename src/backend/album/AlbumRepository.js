4522f7491773df8dceecd9fb85fac84cb6bcd681
Roy Brown <roybrown77@gmail.com>
2024-09-23 16:43:36 -0400

    remove unused files

--- src/backend/album/AlbumRepository.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-var AlbumSchema = new mongoose.Schema({
-    title: String,
-    artist: String,
-    releaseDate: Date,
-});
-module.exports = mongoose.model('Album', AlbumSchema);
