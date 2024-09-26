761c2e5d7e1811dcf82783cf476e2745896217f0
Roy Brown <roybrown77@gmail.com>
2024-09-26 10:10:49 -0400

    convert to functional component

--- src/frontend/src/components/AppLayout.jsx
+++ src/frontend/src/components/AppLayout.jsx
@@ -1,9 +1,5 @@
-import React from 'react';
-import {createBrowserHistory} from 'history';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-
-import get from 'lodash/get';
 
 import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -11,140 +7,106 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 
-const history = createBrowserHistory({forceRefresh: true});
-
-const styles = theme => ({
-    appBar: {
-        boxShadow: 'none',
-        height: '56px',
-        backgroundColor: '#444'
+const styles = (theme) => ({
+  appBar: {
+    boxShadow: 'none',
+    height: '56px',
+    backgroundColor: '#444',
+  },
+  toolBar: {
+    minHeight: '0',
+  },
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroUnit: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  heroContent: {
+    maxWidth: 600,
+    margin: '0 auto',
+    padding: `${theme.spacing(8)}px 0 ${theme.spacing(6)}px`,
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing(3),
+    marginRight: theme.spacing(3),
+    [theme.breakpoints.up(1100 + theme.spacing(6))]: {
+      width: 1100,
+      marginLeft: 'auto',
+      marginRight: 'auto',
     },
-    toolBar: {
-        minHeight: '0'
-    },
-    icon: {
-        marginRight: theme.spacing(2),
-    },
-    heroUnit: {
-        backgroundColor: theme.palette.background.paper,
-    },
-    heroContent: {
-        maxWidth: 600,
-        margin: '0 auto',
-        padding: `${theme.spacing(8)}px 0 ${theme.spacing(6)}px`,
-    },
-    heroButtons: {
-        marginTop: theme.spacing(4),
-    },
-    layout: {
-        width: 'auto',
-        marginLeft: theme.spacing(3),
-        marginRight: theme.spacing(3),
-        [theme.breakpoints.up(1100 + theme.spacing(6))]: {
-            width: 1100,
-            marginLeft: 'auto',
-            marginRight: 'auto',
-        },
-    },
-    cardGrid: {
-        padding: `${theme.spacing(8)}px 0`,
-    },
-    cardMedia: {
-        padding: '50%', // 16:9
-    },
-    footer: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(6),
-        fontFamily: 'Poppins'
-    },
-    navigationUnselected: {
-        fontWeight: '600',
-        fontSize: '16px',
-        color: 'white',
-        textDecoration: 'none'
-    },
-    navigationSelected: {
-        fontWeight: '600',
-        fontSize: '16px',
-        color: 'white',
-        height: '3.5rem',
-        padding: '.5em',
-        border: '2px solid #708090',
-        borderRadius: '6px',
-        textDecoration: 'none'
-    }
+  },
+  cardGrid: {
+    padding: `${theme.spacing(8)}px 0`,
+  },
+  cardMedia: {
+    padding: '50%', // 16:9
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+    fontFamily: 'Poppins',
+  },
+  navigationUnselected: {
+    fontWeight: '600',
+    fontSize: '16px',
+    color: 'white',
+    textDecoration: 'none',
+  },
+  navigationSelected: {
+    fontWeight: '600',
+    fontSize: '16px',
+    color: 'white',
+    height: '3.5rem',
+    padding: '.5em',
+    border: '2px solid #708090',
+    borderRadius: '6px',
+    textDecoration: 'none',
+  },
 });
 
-class AppLayout extends React.Component {
-    constructor(props) {
-        super(props);
-        const isLoggedIn = !!localStorage.getItem('token');
-        const pathname = window.location.pathname;
-
-        this.state = {
-            isLoggedIn,
-            selection: pathname.replace('/','')
-        };
-    }
-
-    handleLogout = async (event) => {
-        event.preventDefault();
-        history.push('/login');
-    };
-
-    handleLogin = async (event) => {
-        event.preventDefault();
-        history.push('/login');
-    };
-
-    handleRegister = async (event) => {
-        event.preventDefault();
-        history.push('/register');
-    };
-
-    handleSelectNavOption = async (event) => {
-        const fieldName = get(event, 'target.name');
-        this.setState(() => {
-            return {selection: fieldName}
-        })
-    };
-
-    render() {
-        const {classes, children} = this.props;
-
-        return (
-            <div>
-                <React.Fragment>
-                    <CssBaseline />
-                    <AppBar position="static" className={classNames(classes.appBar)}>
-                        <Toolbar className={classNames(classes.toolBar)}>
-                            <h3 style={{fontSize: '18px', fontWeight: 'bold', color: '#E2A42B', margin: '15px auto', fontFamily: 'Poppins'}}>
-                                {'Wu-Tang x React Node Template'}
-                            </h3>
-                        </Toolbar>
-                    </AppBar>
-                    <div className={classNames(classes.main)}>
-                        {children}
-                    </div>
-                    <div className={classNames(classes.footer)}>
-                        This web app will retrieve Wu-Tang album covers from Wikipedia and display them.  It's a demo to showcase how to 
-                        build a web app using ReactJs and NodeJs javascript software.  Code can be found <a href="https://www.github.com/roybrown77/wu-tang-react-node-template">here</a> since 
-                        Wu-Tang is for the kids.  Sample music track doesn't appear/play on a mobile device so listen on desktop.  Enjoy.  :D
-                    </div>
-                </React.Fragment>
-            </div>
-        );
-    }
+const AppLayout = ({ classes, children }) => {
+  return (
+    <div>
+      <CssBaseline />
+      <AppBar position="static" className={classNames(classes.appBar)}>
+        <Toolbar className={classNames(classes.toolBar)}>
+          <h3
+            style={{
+              fontSize: '18px',
+              fontWeight: 'bold',
+              color: '#E2A42B',
+              margin: '15px auto',
+              fontFamily: 'Poppins',
+            }}
+          >
+            {'Wu-Tang x React Node Template'}
+          </h3>
+        </Toolbar>
+      </AppBar>
+      <div className={classNames(classes.main)}>{children}</div>
+      <div className={classNames(classes.footer)}>
+        This web app will retrieve Wu-Tang album covers from Wikipedia and display them. It's a demo to showcase how to
+        build a web app using ReactJs and NodeJs javascript software. Code can be found{' '}
+        <a href="https://www.github.com/roybrown77/wu-tang-react-node-template">here</a> since Wu-Tang is for the kids.
+        Sample music track doesn't appear/play on a mobile device so listen on desktop. Enjoy. :D
+      </div>
+    </div>
+  );
 };
 
 AppLayout.propTypes = {
-    classes: PropTypes.object.isRequired,
-    children: PropTypes.any,
-    title: PropTypes.string
+  classes: PropTypes.object.isRequired,
+  children: PropTypes.any,
+  title: PropTypes.string,
 };
 
 AppLayout.defaultProps = {
-    title: ''
+  title: '',
 };
 
-export default (withStyles(styles)(AppLayout));
+export default withStyles(styles)(AppLayout);
--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player';
