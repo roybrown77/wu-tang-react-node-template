5fb90bdecaa25ff97048dcec66200201b058a9e4
Roy Brown <roybrown77@gmail.com>
2024-10-03 08:14:06 -0400

    remove unused configs

--- config.js
+++ config.js
@@ -4,8 +4,4 @@ dotenv.config();
 module.exports = {
 	nodeEnv: process.env.NODE_ENV,
   	httpPort: process.env.PORT,
-  	dbUsername: process.env.DB_USERNAME,
-  	dbPassword: process.env.DB_PASSWORD,
-  	dbName: process.env.DB_NAME,
-  	queueUrl: process.env.CLOUDAMQP_URL || 'amqp://localhost'
 };
