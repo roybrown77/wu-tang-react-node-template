8ed546a5707671aa9d7e7027ccdb99fa1350b861
Roy Brown <roybrown77@gmail.com>
2024-10-14 16:41:36 -0400

    scale pwa capabilities with vite-plugin-pwa

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: false,
+      pwaAssets: { disabled: false, config: true},
+      manifest: {
+        name: 'Wu-Tang x React x Node',
+        short_name: 'Wu x React x Node',
+        description: 'A web app that displays Wu-Tang Clan album covers and info.',
+        theme_color: '#444444',
+        background_color: "#444444",
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,svg,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        /* when using generateSW the PWA plugin will switch to classic */
+        type: 'module',
+      },
+    })
+  ],
   server: {
     open: true,
     proxy: {
